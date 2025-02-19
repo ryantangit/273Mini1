f44b26bd7a249eb4e13d714704ceb054a259406f
Ryan Tan <ryantan.mail@gmail.com>
2025-02-18 18:35:15 -0800

    additional test case

--- test/Records_test.cpp
+++ test/Records_test.cpp
@@ -3,11 +3,14 @@
 void printStringVector(std::vector<std::string> stringVector);
 void testCaseNormal();
 void testCaseInnerComma();
+void testCaseMissingValues();
 
 int main() {
 	testCaseNormal();		
 	std::cout << std::endl;
 	testCaseInnerComma();
+	std::cout << std::endl;
+	testCaseMissingValues();
 }
 
 
@@ -35,6 +38,17 @@ void testCaseInnerComma() {
 	printStringVector(output);
 }
 
+void testCaseMissingValues() {
+	Records testRecord = Records();
+	std::string input = ",,,1,";
+	std::vector<std::string> expectedOutput = {"","","","1",""}; 
+	std::vector<std::string> output = testRecord.parseRecord(input);
+	std::cout << "Test Case #3: Missing Values" << std::endl;
+	std::cout << "Expected: " << std::endl;
+	printStringVector(expectedOutput);
+	std::cout << "Result: " << std::endl;
+	printStringVector(output);
+}
 
 void printStringVector(std::vector<std::string> stringVector) {
 	for (std::string element: stringVector) {
